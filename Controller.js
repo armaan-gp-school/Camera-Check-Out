3873ca8708023e206915ce561c5c2bf50cae3db8
hcps-chauhanjs <115188558+hcps-chauhanjs@users.noreply.github.com>
2025-02-22 09:25:10 -0500

    Test Barcode Scanner

--- Controller.js
+++ Controller.js
@@ -10,6 +10,27 @@ let userInputs = {
     checkoutDate: document.getElementById('checkoutDate')
 };
 
+
+let barcode = '';
+let interval;
+document.addEventListener('keydown', function(evt) {
+    if (interval)
+        clearInterval (interval);
+    if (evt.code == 'Enter') {
+        if (barcode)
+            handleBarcode (barcode);
+        barcode = '';
+        return;
+    }
+    
+    if (evt.key != 'Shift')
+        barcode += evt.key;
+    interval = setInterval(() => barcode = '', 20);
+});
+function handleBarcode (scanned_barcode) {
+    document.querySelector('#idText').innerText = scanned_barcode;
+}
+
 function onClick() {
     model.addItem(new checkOutItem(userInputs.studentID.value, userInputs.equipment.value, userInputs.checkoutDate.value));
 }
